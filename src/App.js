968eaabc6d8f78705e4db5e66f0a98264c3c6ace
Tommy Bakken Mydland <tommy.bakken.mydland@innovasjonnorge.no>
2018-11-08 10:33:04 +0100

    game interface and numbers selection

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Game from './Game';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <header className="App-header">
+       <Game />
+       </header>
+    
+    </div>
+    );
+  }
+}
+
+export default App;
